f436d50561a141e926c4757bfc82fb991d2cff42
thepsycopathlive <79912910+thepsycopathlive@users.noreply.github.com>
2022-01-08 11:20:35 +0530

    Update ufs-qcom.c

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -37,6 +37,8 @@
 #define MAX_PROP_SIZE		   32
 #define VDDP_REF_CLK_MIN_UV        1200000
 #define VDDP_REF_CLK_MAX_UV        1200000
+/* TODO: further tuning for this parameter may be required */
+#define UFS_QCOM_PM_QOS_UNVOTE_TIMEOUT_US	(10000) /* microseconds */
 
 #define UFS_QCOM_DEFAULT_DBG_PRINT_EN	\
 	(UFS_QCOM_DBG_PRINT_REGS_EN | UFS_QCOM_DBG_PRINT_TEST_BUS_EN)
@@ -1528,6 +1530,7 @@ void ufs_enter_h8_disable(struct Scsi_Host *shost)
 
 	hba->caps &= ~UFSHCD_CAP_CLK_GATING;
 	hba->caps &= ~UFSHCD_CAP_HIBERN8_WITH_CLK_GATING;
+	hba->caps &= ~UFSHCD_CAP_CLK_SCALING;
 	hba->caps &= ~UFSHCD_CAP_POWER_COLLAPSE_DURING_HIBERN8;
 
 	hba->ahit = FIELD_PREP(UFSHCI_AHIBERN8_TIMER_MASK, 0);
@@ -1771,8 +1774,8 @@ static void ufs_qcom_pm_qos_unvote_work(struct work_struct *work)
 	group->state = PM_QOS_UNVOTED;
 	ufs_spin_unlock_irqrestore(host->hba->host->host_lock, flags);
 
-pm_qos_update_request(&group->req, PM_QOS_DEFAULT_VALUE);
-
+	pm_qos_update_request_timeout(&group->req,
+		group->latency_us, UFS_QCOM_PM_QOS_UNVOTE_TIMEOUT_US);
 }
 
 static ssize_t ufs_qcom_pm_qos_enable_show(struct device *dev,
@@ -1934,11 +1937,7 @@ static int ufs_qcom_pm_qos_init(struct ufs_qcom_host *host)
 			goto free_groups;
 		}
 
-		ret = of_property_read_u32_index(node,
-			"qcom,pm-qos-cpu-group-latency-us", i,
-			&host->pm_qos.groups[i].latency_us);
-		if (ret)
-			goto free_groups;
+		host->pm_qos.groups[i].latency_us = 1062;
 
 		host->pm_qos.groups[i].req.type = PM_QOS_REQ_AFFINE_CORES;
 		host->pm_qos.groups[i].req.cpus_affine =
